bd5562896ca8fbd7be723327ae45e43c973162a3
dwalentynowicz-opendialog <159320276+dwalentynowicz-opendialog@users.noreply.github.com>
2024-02-14 10:39:04 -0600

    Update amerDemo.js

--- amerDemo.js
+++ amerDemo.js
@@ -1,3 +1,7 @@
+document.getElementById('scenerioSelector').addEventListener('change', function() {
+    document.body.style.backgroundColor = this.value;
+    document.getElementById('scenerioSelected').textContent = this.value;
+});
 document.getElementById('submitBtn').addEventListener('click', function() {
     var url = document.getElementById('urlInput').value;
     if(url) {
